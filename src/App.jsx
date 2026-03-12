9fd59353d74c3e6e53c49377fa91e9bb9056c2ad
srikanth <bodasrikanth2004@gmail.com>
2026-03-12 10:48:14 +0530

    app.jsx code changed because due to errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
 
 function App() {
   return (
     <>
       <header>
         <div id="logo-img">
-          <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
       </header>
@@ -16,3 +14,26 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+// import MainContent from './components/MainContent';
+// import logo from './assets/images/logo.png';
+
+// function App() {
+//   return (
+//     <>
+//       <header>
+//         <div id="logo-img">
+//           <img src={logo} />
+//         </div>
+//         <h1>Learn & Master GitHub Actions</h1>
+//       </header>
+//       <MainContent />
+//     </>
+//   );
+// }
+
+// export default App;
